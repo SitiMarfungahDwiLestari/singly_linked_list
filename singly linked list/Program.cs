f3cbae04caf2d140572c61e891c488ad87296d21
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-11 20:01:07 +0700

    add node to the list

--- singly linked list/Program.cs
+++ singly linked list/Program.cs
@@ -20,6 +20,18 @@ namespace singly_linked_list
         {
             START = null;
         }
+        public void addNode() //add a node to the list
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+        }
     }
 }
 
