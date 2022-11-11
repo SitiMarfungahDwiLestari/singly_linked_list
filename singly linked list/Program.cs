9962f1f97b78d3789e3495178550de974e4dd1e7
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-11 20:17:38 +0700

    add object traverse

--- singly linked list/Program.cs
+++ singly linked list/Program.cs
@@ -62,7 +62,22 @@ namespace singly_linked_list
             newnode = current;
             previous.next = current;
             previous.next = newnode;
-
+        }
+        public void traverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty.\n");
+            else
+            {
+                Console.WriteLine("\nThe records in the list are : ");
+                Node currentNode;
+                for (
+                    currentNode = START; currentNode != null;
+                    currentNode = currentNode.next
+                    )
+                    Console.Write(currentNode.rollNumber + " " + currentNode.name + " \n");
+                Console.WriteLine();
+            }
         }
     }
 }
